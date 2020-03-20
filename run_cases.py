23cdd45bb112c5dfe7b726951b5b51d6bb461bf3
贝克街的捉虫师 <forpeng@foxmail.com>
2020-03-20 16:49:55 +0800

    集成HttpRunner

--- run_cases.py
+++ run_cases.py
@@ -11,10 +11,12 @@ from airtest.cli.parser import runner_parser
 from unittest import TestLoader
 from airtest.report.report import main as report_main
 from airtest.report.report import get_parger as report_parser
+from httprunner.api import HttpRunner
+from httprunner import report
  
 
 # 测试用例
-# from TestCase.hello import CustomCase
+from TestCase.hello import CustomCase
 from TestCase.android_hello import AndroidCase
 
 
@@ -23,6 +25,7 @@ def load_cases():
     suites = list()
     test_loader = TestLoader()
     # 加载用例
+    # suites.append(test_loader.loadTestsFromTestCase(CustomCase))
     suites.append(test_loader.loadTestsFromTestCase(AndroidCase))
 
     # 从用例类列表文件加载用例
@@ -39,7 +42,7 @@ def load_cases():
 def reporter():
     ap = argparse.ArgumentParser()
     ap = report_parser(ap)
-    ap.set_defaults(lang='zh', static_root=os.path.abspath(os.path.join('Core', 'Report')), outfile=os.path.join('Results', 'report.html'),
+    ap.set_defaults(lang='zh', static_root=os.path.join('..', 'Core', 'Report'), outfile=os.path.join('Results', 'report.html'),
                     log_root='Results',  plugins=['poco.utils.airtest.report', 'Core.Report.report'])
     args = ap.parse_args(sys.argv)
     report_main(args)
@@ -47,7 +50,12 @@ def reporter():
 
 if __name__ == '__main__':
     ap = runner_parser()
+    ap.add_argument("--type", required=False, type=str, choices=('gui', 'api'), help="set test type, gui or api", default='gui')
     ap.set_defaults(log='Results', recording=False, device=["Android:///"])
     args = ap.parse_args(sys.argv)
-    run_script(args, load_cases())
-    reporter()
+    if args.type == 'gui':
+        run_script(args, load_cases())
+        reporter()
+    else:
+        summary = HttpRunner(failfast=True).run(os.path.join('TestCase', 'APICase'))
+        report.gen_html_report(summary, report_dir="Results", report_file=os.path.join('Results', 'report_api.html'))
