c03d4ee2dd3d470a6f8499563e5934595cfe9187
pengpeng <pengpeng@pin-dao.cn>
2020-10-14 10:51:56 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='airobots',
-    version='1.0.3',
+    version='1.0.4',
     author='贝克街的捉虫师',
     author_email='forpeng@foxmail.com',
     description='UI Test Automation Framework for Games and Apps on Android/iOS/Windows/Linux/Web',
@@ -31,21 +31,38 @@ setup(
     long_description_content_type='text/markdown',  # Optional (see note above)
     url='https://github.com/BSTester/Airobots',
     license='Apache License 2.0',
-    keywords=['automation', 'automated-test', 'game', 'android', 'ios', 'windows', 'linux'],
+    keywords=['automation', 'automated-test', 'game', 'android', 'ios', 'windows', 'linux', 'web'],
     package_dir={'': 'src'},  # Optional
     packages=find_packages(where='src'),
+    python_requires='>=3.5, <4',
     install_requires=reqs,
     entry_points="""
     [console_scripts]
     airobots = airobots.__main__:main
     """,
     classifiers=[
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish
+        'License :: OSI Approved :: MIT License',
+
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3 :: Only',
     ],
+    project_urls={  # Optional
+        # 'Bug Reports': 'https://github.com/pypa/sampleproject/issues',
+        # 'Funding': 'https://donate.pypi.org',
+        # 'Say Thanks!': 'http://saythanks.io/to/example',
+        'Source': 'https://github.com/BSTester/Airobots',
+    },
 )
