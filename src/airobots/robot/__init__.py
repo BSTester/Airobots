eeb49eff8415dc25ee471bde8adb376463af55a5
BSTester <forpeng@hotmail.com>
2021-03-30 14:50:37 +0800

    修改Library引用方式

---
+++ src/airobots/robot/__init__.py
@@ -0,0 +1,7 @@
+from airobotLibrary import AirSelenium
+from airobotLibrary import AirAppium
+from DatabaseLibrary import DatabaseLibrary as AirDatabase
+from DiffLibrary import DiffLibrary as AirDiff
+from RequestsLibrary import RequestsLibrary as AirRequests
+from ArchiveLibrary import ArchiveLibrary as AirArchive
+from FtpLibrary import FtpLibrary as AirFtp
